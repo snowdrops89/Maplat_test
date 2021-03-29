24c5ee8c86d15ec9b30ee39a567d427527a302fd
雪乃☆雫 <59927104+snowdrops89@users.noreply.github.com>
2021-03-30 06:22:33 +0900

    Add files via upload

---
+++ test_UI/app.js
@@ -0,0 +1,18 @@
+﻿var option = {
+  appid: "Maplat_test_UI",
+  enableHideMarker: true,   //マーカー非表示
+  enableBorder: true,       //地図境界表示
+  enableShare: true,        //共有
+};
+
+Maplat.createObject(option).then(function(app) {
+  const viewpoint = {
+    zoom: 3  //古地図倍率
+  }
+  const ratio = 20;  //透明度(うっすらベース地図が透けるくらい)
+
+  window.setTimeout(function(){
+    app.core.setViewpoint(viewpoint);   //app→app.core
+    app.core.setTransparency(ratio);    //app→app.core
+  }, 1);
+});
